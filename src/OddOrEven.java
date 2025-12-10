c19ab063e84616b8cdd8b04240421b0fdcf308fc
Keshore <keshore.ecom@gmail.com>
2025-12-10 17:13:50 +0530

    Odd or Even

---
+++ src/OddOrEven.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println("The number is even");
+        }else System.out.println("The number is odd");
+        sc.close();
+    }
+}

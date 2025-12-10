c9597cf1b7a93e382fd4ebe5990961cefa8a015f
Keshore <keshore.ecom@gmail.com>
2025-12-10 16:04:15 +0530

    Prime Factors

---
+++ src/PrimeFactors.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class PrimeFactors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n = sc.nextInt();
+
+        while(n<=1){
+            System.out.println("Enter a valid number:");
+            n = sc.nextInt();
+        }
+        for(int i=2; i*i<=n;i++){
+            while(n%i==0){
+                System.out.println(i);
+                n = n/i;
+            }
+        }
+        if(n>1) System.out.println(n);
+        sc.close();
+    }
+}
